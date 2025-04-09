d802b6b1a6d7a0adc6db1c37157f24f0beafe43f
TheLonelySamurai <yayasanouvong@gmail.com>
2025-04-09 15:00:20 -0400

    bankappfin

---
+++ READMETOLOGIN.txt
@@ -0,0 +1,9 @@
+This is my banking application assignment that completely functions with 
+an added capability of saving admin actions.
+
+To sign in to the test accounts the password is the same as the username.
+For example, the password for jdoe123 is jdoe123.
+
+This is because all passwords are saved with their hash values, not their actual values.
+
+To sign in to the admin account, the password is exactly this: my_secure_password114
--- main.py
+++ main.py
@@ -35,6 +35,7 @@ def get_profile_from_login():
     # Check if the username and password are correct
     user = conn.execute(text("SELECT * FROM Accounts WHERE Username = :username"), {'username': username}).first()
     admin = conn.execute(text("SELECT * FROM Admins WHERE Username = :username"), {'username': username}).first()
+    admins = conn.execute(text("SELECT * FROM Admins")).all()
     login = conn.execute(text("SELECT * FROM Login")).first()
     if user:
         success = False
@@ -54,7 +55,7 @@ def get_profile_from_login():
             print(login)
             # Password is correct, redirect to profile page
             success = True
-        return render_template('logging.html', success=success, username=username, approved=True, admin=False, login=login)
+        return render_template('logging.html', success=success, username=username, approved=True, admin=False, login=login, admins=admins)
     elif admin:
         if decrypt_password(admin.Password_hash, password):
             # Update the Login table to store the login information
@@ -67,9 +68,9 @@ def get_profile_from_login():
             )
             login = conn.execute(text("SELECT * FROM Login")).first()
             # Password is correct, redirect to profile page
-            return render_template('logging.html', success=True, username=username, approved=True, admin=True, login=login)
+            return render_template('logging.html', success=True, username=username, approved=True, admin=True, login=login, admins=admins)
         else:
-            return render_template('logging.html', success=False, username=username, approved=True, admin=False, login=login)
+            return render_template('logging.html', success=False, username=username, approved=True, admin=False, login=login, admins=admins)
 @app.route('/profile/<username>', methods=['GET'])
 def get_profile(username=None):
     login = conn.execute(text("SELECT * FROM Login")).first()
@@ -141,21 +142,28 @@ def transcript_get_request():
     admins = conn.execute(text("SELECT * FROM Admins")).all()
     transcripts = conn.execute(text("SELECT * FROM AdminTranscript")).all()
     return render_template('admin_transcript.html', transcript=transcripts, login=login, admins=admins)
-@app.route('/create', methods=['POST'])
-def create_boat():
+
+@app.route('/account_create', methods=['GET'])
+def create_account_get_request():
+    login = conn.execute(text("SELECT * FROM Login")).first()
+    admins = conn.execute(text("SELECT * FROM Admins")).all()
+    return render_template('account_create.html', login=login, admins=admins, error=None, success=None)
+@app.route('/account_create', methods=['POST'])
+def create_account():
     # you can access the values with request.from.name
     # this name is the value of the name attribute in HTML form's input element
     # ex: print(request.form['id'])
     try:
+        hashed = hash_password(request.form['password'])
         conn.execute(
-            text("INSERT INTO boats (id, name, type, owner_id, rental_price) values (:id, :name, :type, :owner_id, :rental_price)"),
+            text(f"INSERT INTO Accounts (Username, Password_hash, fname, lname, SSN, address, phone) values (:username, '{str(hashed)}',  :fname, :lname, :ssn, :address, :phone)"),
             request.form
         )
-        return render_template('boats_create.html', error=None, success="Data inserted successfully!")
+        return render_template('account_create.html', error=None, success="Account Created!")
     except Exception as e:
         error = e.orig.args[1]
         print(error)
-        return render_template('boats_create.html', error=error, success=None)
+        return render_template('account_create.html', error=error, success=None)
     
 @app.route('/transfer', methods=['GET'])
 def transfer_get_request():
@@ -169,19 +177,33 @@ def transfer_amount():
     admins = conn.execute(text("SELECT * FROM Admins")).all()
     print(login.Username)
     try:
+        currentUser = conn.execute(
+            text("SELECT * FROM Accounts WHERE Username = :user"),
+            {'user': login.Username}).first()
+        # Ensure the user has enough balance to transfer
+        if currentUser.balance < float(request.form['amount']):
+            return render_template('transfer.html', login=login, admins=admins, error="Insufficient funds!", success=None)
+        # Ensure the user is not transferring to themselves
+        if currentUser.AccountID == int(request.form['id']):
+            return render_template('transfer.html', login=login, admins=admins, error="Ensure transfer is to an external account!", success=None)
+        # Ensure the user is not transferring to an invalid account
+        if conn.execute(
+            text("SELECT * FROM Accounts WHERE AccountID = :id"),
+            {'id': request.form['id']}
+        ).first() == None:
+            return render_template('transfer.html', login=login, admins=admins, error="Invalid recipient!", success=None)
+        # Update the balance of the sender and receiver
         conn.execute(
             text("UPDATE Accounts SET balance = balance + :amount WHERE AccountID = :id"),
             request.form
         )
-        currentUser = conn.execute(
-            text("SELECT * FROM Accounts WHERE Username = :user"),
-            {'user': login.Username}).first()
         conn.execute(
             text("UPDATE Accounts SET balance = balance - :amount WHERE AccountID = :id"),
             {'id': currentUser.AccountID, 'amount': request.form['amount']}
         )
         # Save the transaction in the transactions table
-        conn.execute(text("INSERT INTO Transactions (trans_date, fromID, toID, amount) VALUES (NOW(), :from_id, :to_id, :amount)"),
+        conn.execute(
+            text("INSERT INTO Transactions (trans_date, fromID, toID, amount) VALUES (NOW(), :from_id, :to_id, :amount)"),
             {
                 'from_id': currentUser.AccountID,
                 'to_id': request.form['id'],
@@ -213,6 +235,15 @@ def deposit_amount():
             text("UPDATE Accounts SET balance = balance + :amount WHERE AccountID = :id"),
             {'id': currentUser.AccountID, 'amount': request.form['amount']}
         )
+        # Log the deposit in the transactions table
+        conn.execute(
+            text("INSERT INTO Transactions (trans_date, fromID, toID, amount) VALUES (NOW(), :from_id, :to_id, :amount)"),
+            {
+                'from_id': currentUser.AccountID,
+                'to_id': currentUser.AccountID,
+                'amount': request.form['amount']
+            }
+        )
         return render_template('deposit.html', login=login, admins=admins, error=None, success="Data updated successfully!")
     except Exception as e:
         error = e
@@ -224,10 +255,15 @@ def deposit_amount():
 def delete_get_request():
     login = conn.execute(text("SELECT * FROM Login")).first()
     admins = conn.execute(text("SELECT * FROM Admins")).all()
-    if login.Username == None:
+    if login == None:
         return render_template('logging.html', success=False, login=login, admins=admins)
     adminUsernames = conn.execute(text("SELECT Username FROM Admins")).all()
-    if login.Username in adminUsernames:
+    isAdmin = False
+    for admin in adminUsernames:
+        if login.Username in admin:
+            isAdmin = True
+            break
+    if isAdmin:
         transactions = conn.execute(text("SELECT * FROM Transactions")).all()
     else:
         user = conn.execute(text("SELECT * FROM Accounts WHERE Username = :username"), {'username': login.Username}).first()
@@ -236,19 +272,6 @@ def delete_get_request():
             {'from_id': user.AccountID}
             ).all()
     return render_template('transactions.html', transactions=transactions, login=login, admins=admins)
-
-@app.route('/delete', methods=['POST'])
-def delete_boat():
-    try:
-        conn.execute(
-            text("DELETE FROM boats WHERE id = :id"),
-            request.form
-        )
-        return render_template('boats_delete.html', error=None, success="Data deleted successfully!")
-    except Exception as e:
-        error = e.orig.args[1]
-        print(error)
-        return render_template('boats_delete.html', error=error, success=None)
 
 
 if __name__ == '__main__':
